0d52d35877a10a6ae7374759692f8b2b8b44ca72
Elton SV <el10savio96@gmail.com>
2019-08-20 20:27:45 +0530

    Modularized Code

---
+++ processing/processing.go
@@ -0,0 +1,77 @@
+package processing
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	iIndex "../invertedIndex"
+)
+
+func RemoveDuplicates(wordList []string) []string {
+	keys := make(map[string]bool)
+	uniqueWords := []string{}
+
+	for _, entry := range wordList {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			uniqueWords = append(uniqueWords, entry)
+		}
+	}
+
+	return uniqueWords
+}
+
+func Preprocessing(wordList []string) []string {
+	ProcessedWordList := []string{}
+
+	for _, word := range wordList {
+		ProcessedWordList = append(ProcessedWordList, strings.ToLower(word))
+	}
+
+	return ProcessedWordList
+}
+
+func Tokenize(Doc string) []string {
+	wordList := []string{}
+
+	r := regexp.MustCompile("[^\\s]+")
+	wordList = r.FindAllString(Doc, -1)
+
+	wordList = Preprocessing(wordList)
+	wordList = RemoveDuplicates(wordList)
+
+	// fmt.Println(wordList)
+	return wordList
+}
+
+func GenerateDocMap(token []string) map[string]bool {
+	docMap := make(map[string]bool)
+
+	for _, word := range token {
+		if _, value := docMap[word]; !value {
+			docMap[word] = true
+		}
+	}
+
+	return docMap
+}
+
+func GenerateInvertedIndex(DocList []string) {
+	globalDocMap := make([]map[string]bool, 0)
+
+	for _, Doc := range DocList {
+		token := Tokenize(Doc)
+		docMap := GenerateDocMap(token)
+		globalDocMap = append(globalDocMap, docMap)
+	}
+
+	invertedIndex := iIndex.CreateInvertedIndex()
+
+	for DocMapIndex, DocMap := range globalDocMap {
+		for DocEntry, _ := range DocMap {
+			invertedIndex.AddItem(DocEntry, DocMapIndex)
+		}
+	}
+	fmt.Println("invertedIndex:", invertedIndex)
+}
