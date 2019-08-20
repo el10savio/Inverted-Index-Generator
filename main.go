023803c117e468bbc2f0a321b7f1c96f7b1caa57
Elton SV <el10savio96@gmail.com>
2019-08-20 21:29:59 +0530

    Refactored Code

--- main.go
+++ main.go
@@ -5,14 +5,23 @@ import (
 )
 
 func main() {
+	// Here we enter a list of sentences
+	// corresponding to text in documents
 	DocList := []string{
 		"new home sales top forecasts",
 		"home sales rise in July",
 		"increase in home sales in July",
 		"July new home sales rise"}
 
+	// Generate the inverted index
 	index := processing.GenerateInvertedIndex(DocList)
 
+	// Search for "Sales" and output the
+	// list of documents containing it
 	processing.Find(index, "Sales")
+
+	// Ability to show that search
+	// outputs values that dont
+	// exist in the documents
 	processing.Find(index, "June")
 }
--- processing/processing.go
+++ processing/processing.go
@@ -8,6 +8,8 @@ import (
 	iIndex "../invertedIndex"
 )
 
+// RemoveDuplicates filters out all duplicate
+// words from each document
 func RemoveDuplicates(wordList []string) []string {
 	keys := make(map[string]bool)
 	uniqueWords := []string{}
@@ -22,9 +24,13 @@ func RemoveDuplicates(wordList []string) []string {
 	return uniqueWords
 }
 
+// Preprocessing converts each word to lowercase
+// TODO: Clean up each word for symbols
 func Preprocessing(wordList []string) []string {
 	ProcessedWordList := []string{}
 
+	// Convert each string to lowercase from
+	// wordList and add to ProcessedWordList
 	for _, word := range wordList {
 		ProcessedWordList = append(ProcessedWordList, strings.ToLower(word))
 	}
@@ -32,19 +38,24 @@ func Preprocessing(wordList []string) []string {
 	return ProcessedWordList
 }
 
+// Tokenize gets the individual words from each
+// document and generates a wordlist
 func Tokenize(Doc string) []string {
 	wordList := []string{}
 
+	// The following regexp finds indivdual
+	//  words in a sentence
 	r := regexp.MustCompile("[^\\s]+")
 	wordList = r.FindAllString(Doc, -1)
 
 	wordList = Preprocessing(wordList)
 	wordList = RemoveDuplicates(wordList)
 
-	// fmt.Println(wordList)
 	return wordList
 }
 
+// GenerateDocMap creates a hash map of
+// each word in the document
 func GenerateDocMap(token []string) map[string]bool {
 	docMap := make(map[string]bool)
 
@@ -57,6 +68,11 @@ func GenerateDocMap(token []string) map[string]bool {
 	return docMap
 }
 
+// GenerateInvertedIndex for each document list
+// gets each word as a token, processes it and
+// generates a hash map for each document
+// using them it then generates the
+// inverted index of all words
 func GenerateInvertedIndex(DocList []string) iIndex.InvertedIndex {
 	globalDocMap := make([]map[string]bool, 0)
 
@@ -66,8 +82,11 @@ func GenerateInvertedIndex(DocList []string) iIndex.InvertedIndex {
 		globalDocMap = append(globalDocMap, docMap)
 	}
 
+	// Create an empty inverted index
 	invertedIndex := iIndex.CreateInvertedIndex()
 
+	// Using the generated hash maps add
+	// each word to the inverted index
 	for DocMapIndex, DocMap := range globalDocMap {
 		for DocEntry, _ := range DocMap {
 			invertedIndex.AddItem(DocEntry, DocMapIndex)
@@ -76,6 +95,10 @@ func GenerateInvertedIndex(DocList []string) iIndex.InvertedIndex {
 	return *invertedIndex
 }
 
+// Find for a given inverted index and search term
+// checks if the term exists and then
+// ouputs the documents the
+// term is in
 func Find(index iIndex.InvertedIndex, searchTerm string) {
 	Term := strings.ToLower(searchTerm)
 
